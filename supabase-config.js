851ee8fbef5a7dfa3071014dd879312ea1a00b21
ADIL CA <badpiggiesbrawlhalla@gmail.com>
2026-05-10 12:01:16 +0530

    Configure Supabase anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.CREATORLINK_SUPABASE = {
   url: "https://wuwvdeqbgjnjxxbhwaie.supabase.co",
-  anonKey: ""
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1d3ZkZXFiZ2puanh4Ymh3YWllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzODE5NjAsImV4cCI6MjA5Mzk1Nzk2MH0.pdYFSpn4b_U1_zE6wYIe_6B-TKiJ-dLaOt_UAvVaUGc"
 };
